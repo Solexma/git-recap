fe3428fc20a5bd1d3e94bbd23872ef4fecdeafca
Marco Orlandin <mipnamic@mipnamic.net>
2026-03-25 00:04:09 +0100

    implement touch command with lazy init

--- src/commands/touch.rs
+++ src/commands/touch.rs
@@ -1,5 +1,24 @@
+use colored::Colorize;
+
 use crate::error::Result;
+use crate::git;
+use crate::registry;
+use crate::report::Report;
 
 pub fn run() -> Result<()> {
-    todo!()
+    let sha = git::initial_commit_sha()?;
+    let root = git::repo_root()?;
+    let name = git::repo_name()?;
+
+    if !registry::is_registered(&sha)? {
+        registry::register(&sha, &root)?;
+    }
+
+    let mut report = Report::load_or_init(root, name, sha)?;
+    report.activity.last_touched = chrono::Local::now().fixed_offset();
+    report.save()?;
+
+    println!("{} touched", "Done.".green().bold());
+
+    Ok(())
 }

f5fba7d5b0118dd35875a2b4425816335e494d45
Marco Orlandin <mipnamic@mipnamic.net>
2026-03-25 00:04:51 +0100

    implement status command

--- src/commands/status.rs
+++ src/commands/status.rs
@@ -1,5 +1,43 @@
+use colored::Colorize;
+
 use crate::error::Result;
+use crate::git;
+use crate::report::Report;
 
 pub fn run() -> Result<()> {
-    todo!()
+    let sha = git::initial_commit_sha()?;
+    let report = Report::load(&sha)?;
+
+    let branch = report
+        .activity
+        .last_commit
+        .as_ref()
+        .map_or("unknown", |c| c.branch.as_str());
+
+    println!("{} ({})", report.repo.name.bold(), branch.cyan());
+
+    if let Some(ref commit) = report.activity.last_commit {
+        println!(
+            "  last commit: {} \u{2014} {}",
+            commit.date.format("%Y-%m-%d %H:%M"),
+            commit.message
+        );
+    }
+
+    println!(
+        "  last touched: {}",
+        report.activity.last_touched.format("%Y-%m-%d %H:%M")
+    );
+
+    if let Some(ref today) = report.activity.today {
+        println!(
+            "  today: {} commits, {} files, {} {}",
+            today.commits,
+            today.files_changed,
+            format!("+{}", today.insertions).green(),
+            format!("-{}", today.deletions).red()
+        );
+    }
+
+    Ok(())
 }

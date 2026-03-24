937189619e9b3c6c729ba5b711b7d10d95e1ec0e
Marco Orlandin <mipnamic@mipnamic.net>
2026-03-25 00:05:08 +0100

    implement info command

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -1,5 +1,36 @@
+use colored::Colorize;
+
+use crate::config;
 use crate::error::Result;
+use crate::git;
+use crate::report::Report;
 
 pub fn run() -> Result<()> {
-    todo!()
+    println!("{}", "git-recap".bold());
+    println!("Version: {}", env!("CARGO_PKG_VERSION"));
+    println!();
+    println!("Hook-driven activity reporter for git repos.");
+    println!("Foundation data layer for gitnapped.");
+    println!();
+    println!("{}", "Author:".cyan());
+    println!("  MiPnamic <mipnamic@mipnamic.net>");
+    println!("  https://github.com/MiPnamic");
+    println!();
+    println!("{}", "Project:".cyan());
+    println!("  https://github.com/Solexma/git-recap");
+    println!("  MIT License - Solexma LLC");
+    println!();
+
+    if let Ok(sha) = git::initial_commit_sha() {
+        println!("{}", "Current project:".cyan());
+        println!("  Root SHA: {}", sha);
+        println!("  Report: {}", config::report_file(&sha).display());
+
+        match Report::load(&sha) {
+            Ok(_) => println!("  Tracked: {}", "yes".green()),
+            Err(_) => println!("  Tracked: {}", "no".yellow()),
+        }
+    }
+
+    Ok(())
 }
